af5f370ba8159fb77f045a1949e2e88e5125c990
wallmartins <wall_martins_@hotmail.com>
2021-01-10 14:30:42 -0300

    need to fix close toggle menu on click a anchor link

--- js/modules/toggle-menu.js
+++ js/modules/toggle-menu.js
@@ -1,3 +1,6 @@
 export default function initToggleMenu() {
- console.log('teste');
+  const closeBtn = document.querySelector('.close');
+  const menuClicked = document.querySelectorAll('.menuContent');
+
+  menuClicked.forEach((link) => link.addEventListener('click', closeBtn))
 }
